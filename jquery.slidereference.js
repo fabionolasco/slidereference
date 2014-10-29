f7954c20d2c6cebe08356b9353fd5c5015454fc9
gomidefabio <gomidefabio@gmail.com>
2014-10-29 09:43:35 -0400

    Improved comments - docs

--- jquery.slidereference.js
+++ jquery.slidereference.js
@@ -1,9 +1,19 @@
 ;
 
+/*jslint browser: true*/
+/*jslint todo: true */
+
 /**
 * Add a slider to website containing an iframe with the URL of your choice.
 * @author Fabio Gomide Nolasco
 * @date 27/Oct/2014
+* @param {jQuery} $
+* @todo Remove CSS3 animation and add jQuery animation to make it work in older browsers
+* @todo Create minified version and put on github
+* @todo Add close button choice
+* @todo Add resize iframe width choice
+* @todo Create way to style iframe
+* @todo Mode event handler attachement to plugin
 **/
 (function ($) {
 
@@ -58,9 +68,9 @@
         $(element).css(css_comand, css_value);
     }
 
-    Slidereference.prototype.init = function(){
+    Slidereference.prototype.init = function () {
 
-        if ($("#slidereference").length){
+        if ($("#slidereference").length) {
 
             $("#slidereference").remove();
             // Change container to accommodate slide
@@ -73,8 +83,8 @@
             this.config.loadingcss.width = this.config.reference_width;
 
             // Create Reference Container
-            var slide = $('<div/>'), 
-                slidereference_loading = $('<div/>'), 
+            var slide = $('<div/>'),
+                slidereference_loading = $('<div/>'),
                 slidereference_bars = $('<div/>'),
                 bars_style = "<style>loading{display:block;width:200px;height:200px;margin:0 auto;border:none;position:relative}.loader,.loader:after,.loader:before{background:#FFF;-webkit-animation:load1 1s infinite ease-in-out;animation:load1 1s infinite ease-in-out;width:1em;height:4em}.loader:after,.loader:before{position:absolute;top:0;content:''}.loader:before{left:-1.5em}.loader{text-indent:-9999em;margin:2em auto;position:relative;font-size:11px;-webkit-animation-delay:-.16s;animation-delay:-.16s}.loader:after{left:1.5em;-webkit-animation-delay:-.32s;animation-delay:-.32s}@-webkit-keyframes load1{0%,100%,80%{box-shadow:0 0 #FFF;height:4em}40%{box-shadow:0 -2em #fff;height:5em}}@keyframes load1{0%,100%,80%{box-shadow:0 0 #FFF;height:4em}40%{box-shadow:0 -2em #fff;height:5em}}</style>",
                 w = this.config.reference_width,
@@ -86,13 +96,13 @@
 
             // Loading bars
             $(slidereference_bars)
-                .prop("id","slidereference_bars")
+                .prop("id", "slidereference_bars")
                 .html('<loading><div class="loader"></div></loading>' + bars_style)
                 .css(this.config.loadingcss);
 
             // Loading screen
             $(slidereference_loading)
-                .prop("id","slidereference_loading")
+                .prop("id", "slidereference_loading")
                 .html(this.config.loading_text + $(slidereference_bars).html())
                 .css(this.config.loadingcss)
                 .css(this.config.animation)
@@ -105,13 +115,13 @@
 
             // Create Slide with iframe
             $(slide)
-                .prop("id","slidereference")
+                .prop("id", "slidereference")
                 .html('<iframe id="slidereference_iframe" src="' + this.config.url + '" style="width:100%;height:100%;"></iframe>')
                 .css(this.config.css)
                 .appendTo(this.element);
 
             // Remove loading after content is loaded
-            $("#slidereference_iframe").bind("load", function(){
+            $("#slidereference_iframe").bind("load", function () {
 
                 // Slide iframe to page
                 window.setTimeout(function () { changeCss('#slidereference', "margin-left", '-' + w); }, 1);
@@ -130,4 +140,4 @@
         return this.first();
     };
 
-}(jQuery));
+}(jQuery, $));
